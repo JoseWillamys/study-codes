c68ec8112315e2370b54eb46baa251c5596653a7
José Willamys <willamysoad@gmail.com>
2026-01-07 16:50:23 -0300

    estrutura condicional Switch case

---
+++ switch-case/switch-case/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    public class  Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string diaSemana;
+
+            Console.WriteLine("Informe um número de 1 a 7:");
+            numero = int.Parse(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    diaSemana = "Domingo";
+                    break;
+                case 2:
+                    diaSemana = "Segunda-feira";
+                    break;
+                case 3:
+                    diaSemana = "Terça-feira";
+                    break;
+                case 4:
+                    diaSemana = "Quarta-feira";
+                    break;
+                case 5:
+                    diaSemana = "Quinta-feira";
+                    break;
+                case 6:
+                    diaSemana = "Sexta-feira";
+                    break;
+                case 7:
+                    diaSemana = "Sábado";
+                    break;
+                default:
+                    diaSemana = "Valor inválido";
+                    break;
+            }
+
+            Console.WriteLine("Dia da semana: " + diaSemana);
+        }
+    }
+}
