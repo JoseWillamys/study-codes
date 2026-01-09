38748468bd97c47bd40e83e894c9f353b0824688
José Willamys <willamysoad@gmail.com>
2026-01-09 15:14:27 -0300

    estrutura condicional

---
+++ exercicio1044/exercicio1044/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leia 2 valores inteiros (A e B). Após, o programa deve mostrar
+            // uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando
+            // se os valores lidos são múltiplos entre si.
+
+            string[] vet;
+            int a, b;
+
+            vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+
+            if (b % a == 0 || a % b == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
