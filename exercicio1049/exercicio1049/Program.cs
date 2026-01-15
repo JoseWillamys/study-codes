7d848ae0727ab8ea9ff97bb9480d13675ae96391
José Willamys <willamysoad@gmail.com>
2026-01-15 15:14:25 -0300

    estrutura condicional

---
+++ exercicio1049/exercicio1049/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace exercicio1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Neste problema, você deverá ler 3 palavras que definem
+            // o tipo de animal possível segundo o esquema abaixo, da
+            // esquerda para a direita.  Em seguida conclua qual dos
+            // animais seguintes foi escolhido, através das três palavras
+            // fornecidas.
+
+            string grupo, classe, alimentacao, animal;
+
+            grupo = Console.ReadLine();
+            classe = Console.ReadLine();
+            alimentacao = Console.ReadLine();
+
+            if (grupo == "vertebrado")
+            {
+                if (classe == "ave")
+                {
+                    if (alimentacao == "carnivoro")
+                    {
+                        animal = "aguia";
+                    }
+                    else
+                    {
+                        animal = "pomba";
+                    }
+                }
+                else
+                {
+                    if (alimentacao == "onivoro")
+                    {
+                        animal = "homem";
+                    }
+                    else
+                    {
+                        animal = "vaca";
+                    }
+                }
+            }
+            else
+            {
+                if (classe == "inseto")
+                {
+                    if (alimentacao == "hematofago")
+                    {
+                        animal = "pulga";
+                    }
+                    else
+                    {
+                        animal = "lagarta";
+                    }
+                }
+                else
+                {
+                    if (alimentacao == "hematofago")
+                    {
+                        animal = "sanguessuga";
+                    }
+                    else
+                    {
+                        animal = "minhoca";
+                    }
+                }
+            }
+            Console.WriteLine(animal);
+        }
+    }
+}
+
