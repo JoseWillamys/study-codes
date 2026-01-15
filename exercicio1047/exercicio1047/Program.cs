caf65151cc0c45727751cb47db6f779f85010d50
José Willamys <willamysoad@gmail.com>
2026-01-15 13:00:29 -0300

    estrutura condicional

---
+++ exercicio1047/exercicio1047/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercico1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hInicial, mInicial, hFinal, mFinal, minutosInicial, minutosFinal, duracao, hora, minuto;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            hInicial = int.Parse(valores[0]);
+            mInicial = int.Parse(valores[1]);
+            hFinal = int.Parse(valores[2]);
+            mFinal = int.Parse(valores[3]);
+
+            // transformando tudo em minutos
+            minutosInicial = hInicial * 60 + mInicial;
+            minutosFinal = hFinal * 60 + mFinal;
+        
+            if (minutosInicial < minutosFinal)
+            {
+               duracao = minutosFinal - minutosInicial;
+            } 
+            else
+            {
+                duracao = (24 * 60 - minutosInicial) + minutosFinal; 
+            }
+
+            hora = duracao / 60;
+            minuto = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + hora + " HORA(S) E " + minuto + " MINUTO(S)");
+        }
+    }
+}
