545e8f4bacabc3be5730f769476954f0fcba3764
José Willamys <willamysoad@gmail.com>
2026-01-21 18:39:55 -0300

    estrutura de repetição

--- exEstruturaRepetitiva01/exEstruturaRepetitiva01/Program.cs
+++ exEstruturaRepetitiva01/exEstruturaRepetitiva01/Program.cs
@@ -5,6 +5,15 @@ namespace exEstruturaRepetitiva01 {
     class Program { 
         static void Main(string[] args) {
 
+            // Leia uma quantidade indeterminada de duplas de
+            // valores inteiros X e Y. Escreva para cada X e Y
+            // uma mensagem que indique se estes valores foram
+            // digitados em ordem crescente ou decrescente.
+
+            // A entrada contém vários casos de teste. Cada caso
+            // contém dois valores inteiros X e Y. A leitura deve
+            // ser encerrada ao ser fornecido valores iguais para X e Y.
+
             int x, y;
 
             string[] vet = Console.ReadLine().Split(' ');
