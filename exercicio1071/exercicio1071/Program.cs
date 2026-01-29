1ffcbb49560a310b912433a0096f9be58e2d813f
José Willamys <willamysoad@gmail.com>
2026-01-29 12:27:24 -0300

    estrutura de repetição ( for )

---
+++ exercicio1071/exercicio1071/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercicio1071 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            int x, y, inicio, final, soma;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y) {
+                inicio = x;
+                final = y;
+            }
+            else {
+                inicio = y;
+                final = x;
+            }
+
+            soma = 0;
+
+            for (int i = inicio + 1; i < final; i++) { 
+                if (i % 2 != 0) {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
