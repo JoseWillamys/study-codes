9048c617005a146b6cd4f52fffb3f1f8061aa8cb
José Willamys <willamysoad@gmail.com>
2026-01-29 12:02:16 -0300

    estrutura de repetição ( for )

---
+++ exercicio1078/exercicio1078/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicio1078 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            int N;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++) {
+                int resultado = i * N;
+                Console.WriteLine(i + " x " + N + " = " + resultado);
+            }
+        }
+    }
+}
