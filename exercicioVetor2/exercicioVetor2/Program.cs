966d6d655144426eaf0f8ceb4b149c2aba9b4ac8
José Willamys <willamysoad@gmail.com>
2026-01-29 19:59:42 -0300

    estrutura de repetição ( for )

---
+++ exercicioVetor2/exercicioVetor2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicioVetor2 { 
+    class Program { 
+        static void Main(string[] args) {
+            int N;
+            int[] numeros;
+
+            N = int.Parse(Console.ReadLine());
+            numeros = new int[N];
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++) {
+                numeros[i] = int.Parse(vet[i]);
+
+                if (numeros[i] < 0) {
+                    Console.WriteLine(numeros[i]);
+                }
+            }
+        }
+    }
+}
