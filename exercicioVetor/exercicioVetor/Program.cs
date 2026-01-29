b47ddce8e9370e2b3467e6ac865827f48eff3c56
José Willamys <willamysoad@gmail.com>
2026-01-29 19:46:17 -0300

    estrutura de repetição (for)

---
+++ exercicioVetor/exercicioVetor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioVetor { 
+    class Program { 
+        static void Main(string[] args) {
+
+            int n;
+            double[] numeros;
+
+            n = int.Parse(Console.ReadLine());
+            numeros = new double[n];
+
+            for (int i = 0; i < n; i++) {
+                numeros[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine(numeros[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
