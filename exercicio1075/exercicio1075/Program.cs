3bf32c69f938ebadb610bd71c16ecf9bc68e3473
José Willamys <willamysoad@gmail.com>
2026-02-02 17:24:34 -0300

    estrutura de repetição (for)

---
+++ exercicio1075/exercicio1075/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercicio1075 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            // Leia um valor inteiro N. Apresente todos os números entre
+            // 1 e 10000 que divididos por N dão resto igual a 2.
+
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10000; i++) { 
+                if ( i % n == 2) { 
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
