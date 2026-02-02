f6725232966ec06af64066fc8d65e3afc9760eb6
José Willamys <willamysoad@gmail.com>
2026-02-02 16:01:30 -0300

    estrutura de repetição (for)

---
+++ exercicio1067/exercicio1067/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercicio1067 { 
+    class Program { 
+        static void Main(string[] args) {
+            int valor;
+
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= valor; i++) { 
+                if ( i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
