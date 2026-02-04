6523f774d86fdbf5ba5e6f54ab404c39cf57475c
José Willamys <willamysoad@gmail.com>
2026-02-04 14:04:08 -0300

    estrutura de repetição (for)

---
+++ exercicio1096/exercicio1096/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercicio1096 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            // Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i % 2 != 0) {
+                    Console.WriteLine("I=" + i + " J=7");
+                    Console.WriteLine("I=" + i + " J=6");
+                    Console.WriteLine("I=" + i + " J=5");
+                }
+            }
+        }
+    }
+}
