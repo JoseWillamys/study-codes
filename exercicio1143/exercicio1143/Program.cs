268383e39247d530cd0ea2a6dfb3f4bdccba7478
José Willamys <willamysoad@gmail.com>
2026-02-04 15:18:36 -0300

    estrutura de repetição (for)

---
+++ exercicio1143/exercicio1143/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio1143 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            // Escreva um programa que leia um valor inteiro N (1 < N < 1000).
+            // Este N é a quantidade de linhas de saída que serão apresentadas
+            // na execução do programa.
+
+            // O arquivo de entrada contém um número inteiro positivo N.
+
+
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine(i + " " + Math.Pow(i, 2) + " " + Math.Pow(i, 3));
+            }
+        }
+    }
+}
