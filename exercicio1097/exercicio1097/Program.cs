85862a2866dacb5cc61784ff6a0e0f8146cea2bf
José Willamys <willamysoad@gmail.com>
2026-02-04 14:23:20 -0300

    estrutura de repetição (for)

---
+++ exercicio1097/exercicio1097/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicio1097
+{
+    class Program {
+        static void Main(string[] args)  {
+
+            // Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+            int j1, j2, j3;
+            j1 = 7;
+            j2 = 6;
+            j3 = 5;
+
+            for (int i = 1; i < 10; i++) {
+                
+                if (i % 2 != 0) {
+                    Console.WriteLine("I=" + i + " J=" + j1);
+                    Console.WriteLine("I=" + i + " J=" + j2);
+                    Console.WriteLine("I=" + i + " J=" + j3);
+                    j1 += 2;
+                    j2 += 2;
+                    j3 += 2;
+                }
+            }
+        }
+    }
+}
---
+++ exercicio1116/exercicio1116/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio1116 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            // Escreva um algoritmo que leia 2 números e imprima o resultado da divisão
+            // do primeiro pelo segundo. Caso não for possível mostre a mensagem “divisao
+            // impossivel” para os valores em questão.
+
+            // A entrada contém um número inteiro N. Este N será a quantidade de pares de
+            // valores inteiros(X e Y) que serão lidos em seguida.
+
+
+            int n, x, y;
+            double resultado;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+
+                if (y == 0) {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else {
+                    resultado = (double)x / (double)y;
+                    Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
