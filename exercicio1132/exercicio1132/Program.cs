10cb4c5b577caa0b463034959d52c2fd7d0035d0
José Willamys <willamysoad@gmail.com>
2026-02-04 14:47:52 -0300

    estrutura de repetição (for)

---
+++ exercicio1132/exercicio1132/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercicio1132 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            // Escreva um algoritmo que leia 2 valores inteiros X e Y calcule a soma dos
+            // números que não são múltiplos de 13 entre X e Y, incluindo ambos.
+
+            // O arquivo de entrada contém 2 valores inteiros quaisquer,
+            // não necessariamente em ordem crescente.
+
+            int x, y, soma, min, max;
+
+            soma = 0;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                min = x;
+                max = y;
+            }
+            else { 
+                min = y; max = x;
+            }
+
+            for (int i = min; i <= max; i++) { 
+                if (i % 13 != 0) {
+                    soma += i;  
+                }
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
