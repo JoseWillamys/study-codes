2329fb5e1c56f918db555a7d069d7e82ab2f4a84
José Willamys <willamysoad@gmail.com>
2026-02-12 13:31:48 -0300

    estrutura de repetição (for)

---
+++ exercicio1155/exercicio1155/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio1155 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            // Escreva um algoritmo para calcular e escrever o valor de S,
+            // sendo S dado pela fórmula:
+            // S = 1 + 1 / 2 + 1 / 3 + … +1 / 100
+
+            double s = 0.0;
+
+            for (int i = 1; i <= 100; i++) {
+                s = s + 1.0 / i;
+            }
+            
+            Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
